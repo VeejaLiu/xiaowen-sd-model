2172a70d74ec18e0c1ababb637aa7821d3f0749d
VeejaLiu <veejaliu@gmail.com>
2023-09-23 03:12:52 +0800

    改变生成的图片大小

--- src/service/stable_diffusion_service.py
+++ src/service/stable_diffusion_service.py
@@ -9,15 +9,25 @@ from diffusers import StableDiffusionPipeline
 print(os.get_exec_path())
 pipe = StableDiffusionPipeline.from_single_file(
     """src//service//models//v2-1_768-ema-pruned.ckpt""",
+    transformers=[
+      torchvision.transforms.Resize(512),
+    ],
     cache_dir='./models/')
 pipe = pipe.to("cuda")
+
+
 # pipe.enable_attention_slicing()
 
 
 def draw_with_prompt(prompt="A painting of a cat"):
     print(f"[draw_with_prompt] prompt: '{prompt}'. Start drawing...")
     start_time = datetime.now()
-    images = pipe(prompt).images
+    images = pipe(
+        prompt=prompt,
+        height=512,
+        width=512,
+        # num_inference_steps=100,
+    ).images
     image = images[0]
     end_time = datetime.now()
     print(f"[draw_with_prompt] Done drawing. Elapsed time: {end_time - start_time}")
