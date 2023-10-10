384dae4de0d4221769fa3d6b65950014e41ad468
VeejaLiu <veejaliu@gmail.com>
2023-10-10 21:25:28 +0800

    Add env config

---
+++ env_config.py
@@ -0,0 +1,15 @@
+BAIDU_TRANSLATE_CONFIG = {
+    "APP_ID": '20191210000364692',
+    "APP_KEY": '9YtnRnV1x6NKVlufaiFc',
+    "ENDPOINT": 'http://api.fanyi.baidu.com',
+    "PATH": '/api/trans/vip/translate'
+}
+
+MINIO_CONFIG = {
+    "MINIO_ENDPOINT": '127.0.0.1',
+    "MINIO_PORT": 9000,
+    "MINIO_USE_SSL": False,
+    "MINIO_ACCESS_KEY": 'fwxPczjUDixUPv7AdzmB',
+    "MINIO_SECRET_KEY": 'GfVWDCSa2BRmrkLaNbYRWn6pruybI0ZlBigD7H5E',
+    "MINIO_BUCKET_NAME": 'pictures',
+}
