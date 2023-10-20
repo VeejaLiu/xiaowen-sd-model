e1cecbd62e820610f17e74da6d94ef46c9de70c1
VeejaLiu <veejaliu@gmail.com>
2023-10-20 17:13:07 +0800

    add constant

---
+++ src/constant/TattooStyles.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class TattooStyles(Enum):
+    BLACK_WORK = 1
+    DOT_WORK = 2
+    GEOMETRIC = 3
+    WATERCOLOR = 4
+    REALISM = 5
+    NEO_TRADITIONAL = 6
+    NEW_SCHOOL = 7
+    JAPANESE = 8
+    TRIBAL = 9
+    LETTERING = 10
+    TRASH_POLKA = 11
+
+
