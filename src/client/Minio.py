c3f9de0cb16df3f31f732fb6ee12f8a36165c602
VeejaLiu <veejaliu@gmail.com>
2023-10-27 17:08:43 +0800

    Add new function - put_in_minio_by_file_object

--- src/client/Minio.py
+++ src/client/Minio.py
@@ -15,7 +15,21 @@ minio_client = Minio(
 )
 
 
-def put_in_minio(object_name, file_path):
+def put_in_minio_by_file_object(object_name, file_object):
+    try:
+        result = minio_client.put_object(
+            bucket_name=MINIO_CONFIG["MINIO_BUCKET_NAME"],
+            object_name=object_name,
+            data=file_object,
+            length=-1,
+            part_size=5 * 1024 * 1024  # 5MB
+        )
+        return f"/{result.bucket_name}/{result.object_name}"
+    except S3Error as exc:
+        print("error occurred.", exc)
+
+
+def put_in_minio_by_file_path(object_name, file_path):
     try:
         result = minio_client.fput_object(
             bucket_name=MINIO_CONFIG["MINIO_BUCKET_NAME"],
@@ -37,7 +51,7 @@ if __name__ == "__main__":
         current_directory = os.getcwd()
         print(f"Current directory: {current_directory}")
 
-        result = put_in_minio("image.jpg", "src/service/result/image.jpg")
+        result = put_in_minio_by_file_path("image.jpg", "src/service/result/image.jpg")
         print(result.etag)
     except S3Error as exc:
         print("error occurred.", exc)
