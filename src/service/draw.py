337394a1422f2cd5722aaae1c5f9e1ccaaaf8d96
VeejaLiu <veejaliu@gmail.com>
2023-10-27 17:08:18 +0800

    complete draw process

--- src/service/draw.py
+++ src/service/draw.py
@@ -1,3 +1,11 @@
+import base64
+import io
+from datetime import datetime
+
+import requests
+
+from env_config import SD_API_CONFIG
+from src.client.Minio import put_in_minio_by_file_path, MINIO_URL, put_in_minio_by_file_object
 from src.constant.TattooStyles import TattooStyles
 from src.lib.logger import logger
 from src.service.prompt import handle_prompt
@@ -11,19 +19,29 @@ async def draw_with_prompt(prompt: str, style: TattooStyles):
     logger.info(f"[draw_with_prompt] Drawing with prompt: {prompt}, style: {style}")
 
     # handle prompt
-    prompt = handle_prompt(style, prompt)
+    new_prompt = handle_prompt(style, prompt)
+    logger.info(f"[draw_with_prompt] Final prompt: {new_prompt}")
 
-    # result_paths = []
-    #
-    # # 上传图片到minio
-    # time_string = datetime.now().isoformat()
-    # for i in range(0, len(image_names)):
-    #     image = image_names[i]
-    #     image_path = f"src/service/result/{image}"
-    #     # add timestamp to image name
-    #     image_object_name = f"{time_string}_{i + 1}_{image}"
-    #     result = put_in_minio(image_object_name, image_path)
-    #     logger.info(f"[draw] Saved image to {result}")
-    #     result_paths.append('http://' + MINIO_URL + result)
-    # logger.info(f"[draw] Done drawing.")
-    # return result_paths
+    url = SD_API_CONFIG['URL']
+    headers = {"content-type": "application/json"}
+    payload = {'prompt': new_prompt}
+    response = requests.request("POST", url, json=payload, headers=headers)
+    response.raise_for_status()
+    result = response.json()
+
+    images_data = result['images']
+
+    # 上传图片到minio
+    time_string = datetime.now().isoformat()
+    result_paths = []
+    # for image_base64 in images_data:
+    for i in range(len(images_data)):
+        image_data = base64.b64decode(images_data[i])
+        image_data = io.BytesIO(image_data)
+        image_object_name = f"{time_string}_{i + 1}.png"
+        result = put_in_minio_by_file_object(image_object_name, image_data)
+        logger.info(f"[draw_with_prompt] Saved image to {result}")
+        result_paths.append('http://' + MINIO_URL + result)
+
+    logger.info(f"[draw_with_prompt] Done drawing.")
+    return result_paths
